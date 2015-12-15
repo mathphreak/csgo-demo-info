5a436172af8f563d5708226cb1deb78cf15be092
Matt Horn <mathphreak@gmail.com>
2015-12-14 18:58:31 -0600

    Begin initial skeleton

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// var lodash = require('lodash')
+var package_info = require('../package.json')
+
+module.exports = {
+  version: package_info.version,
+  parse () {
+    // TODO literally all the work
+    return 2
+  }
+}
